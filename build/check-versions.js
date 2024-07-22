76ef2823df05d6fe2ed6c7470b60006777799669
taeng <xodn0720@naver.com>
2024-07-22 15:14:20 +0900

    update

--- build/check-versions.js
+++ build/check-versions.js
@@ -1,10 +1,11 @@
-'use strict'
-const chalk = require('chalk')
-const semver = require('semver')
-const packageConfig = require('../package.json')
-const shell = require('shelljs')
-function exec (cmd) {
-  return require('child_process').execSync(cmd).toString().trim()
+import chalk from 'chalk';
+import semver from 'semver';
+import { execSync } from 'child_process';
+import packageConfig from '../package.json' assert { type: 'json' };
+import shell from 'shelljs';
+
+function exec(cmd) {
+  return execSync(cmd).toString().trim();
 }
 
 const versionRequirements = [
@@ -13,37 +14,32 @@ const versionRequirements = [
     currentVersion: semver.clean(process.version),
     versionRequirement: packageConfig.engines.node
   }
-]
+];
 
 if (shell.which('npm')) {
   versionRequirements.push({
     name: 'npm',
     currentVersion: exec('npm --version'),
     versionRequirement: packageConfig.engines.npm
-  })
+  });
 }
 
-module.exports = function () {
-  const warnings = []
-  for (let i = 0; i < versionRequirements.length; i++) {
-    const mod = versionRequirements[i]
+export default function () {
+  const warnings = [];
+  for (const mod of versionRequirements) {
     if (!semver.satisfies(mod.currentVersion, mod.versionRequirement)) {
-      warnings.push(mod.name + ': ' +
-        chalk.red(mod.currentVersion) + ' should be ' +
-        chalk.green(mod.versionRequirement)
-      )
+      warnings.push(`${mod.name}: ${chalk.red(mod.currentVersion)} should be ${chalk.green(mod.versionRequirement)}`);
     }
   }
 
   if (warnings.length) {
-    console.log('')
-    console.log(chalk.yellow('To use this template, you must update following to modules:'))
-    console.log()
-    for (let i = 0; i < warnings.length; i++) {
-      const warning = warnings[i]
-      console.log('  ' + warning)
+    console.log('');
+    console.log(chalk.yellow('이 템플릿을 사용하려면 다음 모듈을 업데이트해야 합니다:'));
+    console.log();
+    for (const warning of warnings) {
+      console.log(`  ${warning}`);
     }
-    console.log()
-    process.exit(1)
+    console.log();
+    process.exit(1);
   }
 }
